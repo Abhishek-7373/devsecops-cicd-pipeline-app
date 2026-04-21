b2706284df2e51def3c1db477a0d8bd672bd62cc
Abhishek Makwana <abhishekmakwana73@gmail.com>
2026-04-21 14:57:28 +0530

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 // Change this value to test CI/CD
-const version = "v5- chat notification updates";
+const version = "v6- end to end updates";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
